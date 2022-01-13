7b08240ba3f50619f16e77b50e28cd8619fa8d2e
pakdo20 <u20181b515@upc.edu.pe>
2022-01-13 00:46:14 -0500

    Rwwww

--- src/main/java/pe/chala/gatito/EjemploGatitoApplication.java
+++ src/main/java/pe/chala/gatito/EjemploGatitoApplication.java
@@ -2,8 +2,10 @@ package pe.chala.gatito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"pe.chala.gatito.controllers"})
 public class EjemploGatitoApplication {
 
 	public static void main(String[] args) {
